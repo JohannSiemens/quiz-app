b2f752f8dd80a09406fc99b5d7e753852cbdae59
JohannSiemens <johann.siemens85@gmx.de>
2023-04-04 11:20:49 +0200

    implemented JS function for answer-button

--- javascript/script.js
+++ javascript/script.js
@@ -3,14 +3,16 @@ const cardOneAnswerButton = document.querySelector(
   '[data-js="card-answer-button1"]'
 );
 const cardOneAnswerToggle = document.querySelector('[data-js="card1-answer"]');
-const cardBookmarkToggle = document.querySelector(
+const cardTwoAnswerToggle = document.querySelector('[data-js="card2-answer"]');
+
+/*const cardBookmarkToggle = document.querySelector(
   '[data-js="card-bookmark-card1"]'
 );
 const cardBookmarkHide = document.querySelector(
   '[data-js="card-bookmark-hide"]'
-);
+);*/
 const cardAnswerButtonText = "Show Answer";
-
+/*
 cardOneAnswerButton.addEventListener("click", () => {
   cardOneAnswerToggle.classList.toggle("card__answer--hide");
   if (cardOneAnswerButton.textContent.includes(cardAnswerButtonText)) {
@@ -19,7 +21,21 @@ cardOneAnswerButton.addEventListener("click", () => {
     cardOneAnswerButton.textContent = cardAnswerButtonText;
   }
   console.log("Hide toggle switched");
-});
+});*/
+
+function hideAnswer(button, answer) {
+  answer.classList.toggle("card__answer--hide");
+  if (button.textContent.includes(cardAnswerButtonText)) {
+    button.textContent = "Hide Answer";
+  } else {
+    button.textContent = cardAnswerButtonText;
+  }
+  console.log("Hide toggle switched");
+}
+
+cardOneAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardOneAnswerButton, cardOneAnswerToggle)
+);
 
 //Die Funktionalität ist grundsätzlich gegeben. Ich konnte auf index.html damit die
 //2. Card an und ausblenden. Es gibt wohl noch einen Trick, wie man von

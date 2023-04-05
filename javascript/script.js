0244dcc59d85598b6caa9700f75f339143430d99
JohannSiemens <johann.siemens85@gmx.de>
2023-04-05 16:32:36 +0200

    cleaned js structure based on coaches feedback

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,18 @@ const cardOneAnswerButton = document.querySelector(
 );
 const cardOneAnswerToggle = document.querySelector('[data-js="card1-answer"]');
 
+cardOneAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardOneAnswerButton, cardOneAnswerToggle)
+);
+
 const cardTwoAnswerButton = document.querySelector(
   '[data-js="card-answer-button2"]'
 );
 const cardTwoAnswerToggle = document.querySelector('[data-js="card2-answer"]');
+
+cardTwoAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardTwoAnswerButton, cardTwoAnswerToggle)
+);
 
 const cardThreeAnswerButton = document.querySelector(
   '[data-js="card-answer-button3"]'
@@ -16,20 +24,36 @@ const cardThreeAnswerToggle = document.querySelector(
   '[data-js="card3-answer"]'
 );
 
+cardThreeAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardThreeAnswerButton, cardThreeAnswerToggle)
+);
+
 const cardFourAnswerButton = document.querySelector(
   '[data-js="card-answer-button4"]'
 );
 const cardFourAnswerToggle = document.querySelector('[data-js="card4-answer"]');
+
+cardFourAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardFourAnswerButton, cardFourAnswerToggle)
+);
 
 const cardFiveAnswerButton = document.querySelector(
   '[data-js="card-answer-button5"]'
 );
 const cardFiveAnswerToggle = document.querySelector('[data-js="card5-answer"]');
 
+cardFiveAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardFiveAnswerButton, cardFiveAnswerToggle)
+);
+
 const cardSixAnswerButton = document.querySelector(
   '[data-js="card-answer-button6"]'
 );
 const cardSixAnswerToggle = document.querySelector('[data-js="card6-answer"]');
+
+cardSixAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardSixAnswerButton, cardSixAnswerToggle)
+);
 
 const cardSevenAnswerButton = document.querySelector(
   '[data-js="card-answer-button7"]'
@@ -38,11 +62,19 @@ const cardSevenAnswerToggle = document.querySelector(
   '[data-js="card7-answer"]'
 );
 
+cardSevenAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardSevenAnswerButton, cardSevenAnswerToggle)
+);
+
 const cardEightAnswerButton = document.querySelector(
   '[data-js="card-answer-button8"]'
 );
 const cardEightAnswerToggle = document.querySelector(
   '[data-js="card8-answer"]'
+);
+
+cardEightAnswerButton.addEventListener("click", () =>
+  hideAnswer(cardEightAnswerButton, cardEightAnswerToggle)
 );
 
 const cardAnswerButtonText = "Show Answer";
@@ -56,35 +88,3 @@ function hideAnswer(button, answer) {
   }
   console.log("Hide toggle switched");
 }
-
-cardOneAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardOneAnswerButton, cardOneAnswerToggle)
-);
-
-cardTwoAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardTwoAnswerButton, cardTwoAnswerToggle)
-);
-
-cardThreeAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardThreeAnswerButton, cardThreeAnswerToggle)
-);
-
-cardFourAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardFourAnswerButton, cardFourAnswerToggle)
-);
-
-cardFiveAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardFiveAnswerButton, cardFiveAnswerToggle)
-);
-
-cardSixAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardSixAnswerButton, cardSixAnswerToggle)
-);
-
-cardSevenAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardSevenAnswerButton, cardSevenAnswerToggle)
-);
-
-cardEightAnswerButton.addEventListener("click", () =>
-  hideAnswer(cardEightAnswerButton, cardEightAnswerToggle)
-);
